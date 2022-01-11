64eb2e46df49c0d6fcc6c1491f562eda20b1adb6
Wictor Ducla <wictor.ducla14@gmail.com>
2022-01-11 10:55:56 -0300

    Primeira classe: Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,13 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+		
 	}
-
 }
