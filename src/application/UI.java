0aa6cb9efc5aeb78b6f1a0569eb75f2cc78c0592
Wictor Ducla <wictor.ducla14@gmail.com>
2022-01-31 11:53:49 -0300

    Logica de xequemate

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,9 +59,15 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turno: " + chessMatch.getTurn());
-		System.out.println("Esperando jogador: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("XEQUE!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Esperando jogador: " + chessMatch.getCurrentPlayer());
+			if(chessMatch.getCheck()) {
+				System.out.println("XEQUE!");
+			}
+		}
+		else {
+			System.out.println("XEQUEMATE!");
+			System.out.println("Ganhador: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
